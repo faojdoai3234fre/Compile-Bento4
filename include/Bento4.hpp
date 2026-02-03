b6db7299371f7df495b2dbc32031e91c86f6d12e
faojdoai3234fre <faojdoai3234fre@protonmail.com>
2026-02-03 18:12:36 +0000

    Update Bento4.hpp

--- include/Bento4.hpp
+++ include/Bento4.hpp
@@ -18,7 +18,7 @@ public:
 	/// <param name="data">The encrypted data to decrypt. The decrypted data will be stored in this byte array. If the decryption fails, the original input data is untouched.</param>
 	/// <param name="keyId">The key ID. The key ID must be hex-encoded and 16 bytes long (32 characters).</param>
 	/// <param name="key">The decryption key. The key must be hex-encoded and 16 bytes long (32 characters).</param>
-  /// <returns>True if the operation succeeded, false otherwise.</returns>
+	/// <returns>True if the operation succeeded, false otherwise.</returns>
 	static bool decrypt(QByteArray& data, const QString& keyId, const QString& key) noexcept;
 
   /// <summary>Decrypt CENC-encrypted data. The decryption is hardware-accelerated using AES-NI.</summary>
